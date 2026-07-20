79463de8faf97962a889796cfd59a2cb77380e88
Tawfik <tawfik.elzahar1@gmail.com>
2026-07-20 23:01:24 +0300

    Fix dynamic import

--- app/pareto/page.tsx
+++ app/pareto/page.tsx
@@ -2,11 +2,6 @@ import dynamic from 'next/dynamic'
 
 const ParetoChart = dynamic(() => import('@/components/ParetoChart'), {
   ssr: false,
-  loading: () => (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', background: '#0a0f1e', color: '#6b89b4' }}>
-      Loading chart...
-    </div>
-  ),
 })
 
 export default function ParetoPage() {
